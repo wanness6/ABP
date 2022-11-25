a3125eb3cbe1bf8d540940545ddf4f59c258021f
wanness6 <38040227+wanness6@users.noreply.github.com>
2022-11-25 11:06:33 +0100

    first commit

---
+++ force-app/main/default/lwc/hisoryTracking/hisoryTracking.js
@@ -0,0 +1,16 @@
+import { LightningElement, wire } from 'lwc';
+import ACCOUNT_ID from '@salesforce/schema/Account.Id';
+import OLD_VALUE from '@salesforce/schema/Asset_History__c.OldValue__c';
+import NEW_VALUE from '@salesforce/schema/Asset_History__c.NewValue__c';
+import getAssetHistory from '@salesforce/apex/AssetHistory.getAssetHistory';
+const COLUMNS = [
+    { label: 'Account Id', fieldName: ACCOUNT_ID.fieldApiName, type: 'text' },
+    { label: 'Old value', fieldName: OLD_VALUE.fieldApiName, type: 'text' },
+    { label: 'New value', fieldName: NEW_VALUE.fieldApiName, type: 'text' }
+    
+];
+export default class hisoryTracking extends LightningElement {
+    columns = COLUMNS;
+    @wire(getAssetHistory)
+    assetHistory;
+}
